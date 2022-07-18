ce754683530380bd27f2fcca4c95fdf204a841f7
koliberdapv <canrando4567@gmail.com>
2022-07-18 18:23:47 +0300

    template string/literals

---
+++ JavaScript/ES6 Module/template_strings_literals.js
@@ -0,0 +1,37 @@
+// Template Literals
+
+// const person = {
+//   name: 'kyle',
+//   job: 'developer',
+//   hobbies: ['surfing', 'baking', 'bowling'],
+// };
+
+// const result = document.getElementById('result');
+
+// result.innerHTML = '<h2>' + person.name + '</h2>' + '<p>' + person.job + '</p>';
+
+// result.innerHTML = `<h2>${person.name}</h2> <p>${person.job}</p>
+// <ul>${person.hobbies
+//   .map((item) => {
+//     return `<li>${item}</li>`;
+//   })
+//   .join('')}</ul>
+// `;
+
+// Tagged tepmlate literals
+
+const author = 'some author';
+const statement = 'some statement';
+
+const quote = highlight`Here is the ${statement} by ${author} and it could not be more true`;
+console.log(quote);
+
+const result = document.getElementById('result');
+result.innerHTML = quote;
+
+function highlight(text, ...vars) {
+  const awesomeText = text.map((item, index) => {
+    return `${item} <strong class="blue">${vars[index] || ''}</strong>`;
+  });
+  return awesomeText.join('');
+}
