b20c712125ae81c5d2a595d60ad9c131f8bb80b8
koliberdapv <canrando4567@gmail.com>
2022-08-11 12:35:40 +0300

    dad jokes (fetch, async/await, try/catch, throw)

---
+++ JavaScript/Projects/Dad jokes/app.js
@@ -0,0 +1,27 @@
+const url = 'https://icanhazdadjoke.com/';
+
+const btn = document.querySelector('.btn');
+const result = document.querySelector('.result');
+
+btn.addEventListener('click', () => fetchDadJoke());
+
+const fetchDadJoke = async () => {
+  result.textContent = 'loading...';
+  try {
+    const resp = await fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'User-Agent': 'learning app',
+      },
+    });
+    if (!resp.ok) {
+      throw new Error('There was an error');
+    }
+    const data = await resp.json();
+    result.textContent = data.joke;
+  } catch (error) {
+    result.textContent = 'There was an error';
+  }
+};
+
+fetchDadJoke();
