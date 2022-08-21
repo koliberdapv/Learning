605567e2f43c528e8d52531601ce23b262612129
koliberdapv <canrando4567@gmail.com>
2022-08-21 21:30:17 +0300

    single page

--- JavaScript/Projects/Cocktails/app.js
+++ JavaScript/Projects/Cocktails/app.js
@@ -1,6 +1,7 @@
 import get from './src/getElement.js';
 import presentDrinks from './src/presentDrinks.js';
 import './src/searchForm.js';
+import './src/toggleLoading.js';
 
 const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
--- JavaScript/Projects/Cocktails/src/fetchDrinks.js
+++ JavaScript/Projects/Cocktails/src/fetchDrinks.js
@@ -1,4 +1,7 @@
+import { showLoading } from './toggleLoading.js';
+
 const fetchDrinks = async (url) => {
+  showLoading();
   try {
     const responce = await fetch(url);
     const data = await responce.json();
