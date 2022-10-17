e2cc6789a3bb180d4720652f5a4a9b7a228dab94
koliberdapv <canrando4567@gmail.com>
2022-10-17 12:28:52 +0300

    day 2

--- JavaScript/Projects/Comfy store/src/cart/addToCartDOM.js
+++ JavaScript/Projects/Comfy store/src/cart/addToCartDOM.js
@@ -1,5 +1,9 @@
 import { formatPrice, getElement } from '../utils.js';
+import { openCart } from './toggleCart.js';
 
-const addToCartDOM = () => {};
+const addToCartDOM = (id) => {
+  console.log(id);
+  openCart();
+};
 
 export default addToCartDOM;
