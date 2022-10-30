d217317cbb72b89d8848bb6ca1a80988f6b430b0
koliberdapv <canrando4567@gmail.com>
2022-10-30 20:57:03 +0200

    itteration over an array ofobjects

--- React/study/tutorial/src/index.js
+++ React/study/tutorial/src/index.js
@@ -3,42 +3,42 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 
-const firstBook = {
-  img: 'https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_ML2_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-};
-
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/51m34-qLjUL._SY291_BO1,204,203,200_QL40_ML2_.jpg',
-  title: 'It Ends with Us: A Novel',
-  author: 'Colleen Hoover',
-};
+const books = [
+  {
+    img: 'https://m.media-amazon.com/images/I/51p2SDOCV9L._SX218_BO1,204,203,200_QL40_ML2_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/51m34-qLjUL._SY291_BO1,204,203,200_QL40_ML2_.jpg',
+    title: 'It Ends with Us: A Novel',
+    author: 'Colleen Hoover',
+  },
+  {
+    img: 'https://m.media-amazon.com/images/I/41ZLnc34EiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+    title: 'Verity',
+    author: 'Colleen Hoover',
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((item) => {
+        return <Book aBook={item}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
+  const { img, title, author } = props.aBook;
   console.log(props);
   return (
     <article className="book">
-      <img src={props.img} alt="bear" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
