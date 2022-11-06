4ce6f437c31a4b2ea22e1acb77fb331b788212ba
koliberdapv <canrando4567@gmail.com>
2022-11-06 21:17:09 +0200

    useState for an array and an object

--- React/react-advanced-v18-main/src/tutorial/1-useState/setup/3-useState-array.js
+++ React/react-advanced-v18-main/src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,51 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  // const [people, setPeople] = React.useState(data);
+  // return (
+  //   <>
+  //     {people.map((person) => {
+  //       const { id, name } = person;
+  //       const removeItem = (id) => {
+  //         let newPeople = people.filter((person) => person.id !== id);
+  //         setPeople(newPeople);
+  //       };
+  //       return (
+  //         <div key={id} className="item">
+  //           <h4>{name}</h4>
+  //           <button onClick={() => removeItem(id)}>remove</button>
+  //         </div>
+  //       );
+  //     })}
+  //     <button className="btn" onClick={() => setPeople([])}>
+  //       clear items
+  //     </button>
+  //   </>
+  // );
+
+  const [people, setPeople] = React.useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        const removeItem = (id) => {
+          let newPeople = people.filter((person) => {
+            return person.id !== id;
+          });
+          setPeople(newPeople);
+        };
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        remove all
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
--- React/react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
+++ React/react-advanced-v18-main/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = React.useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  });
+
+  const [name, setName] = React.useState('peter');
+  const [age, setAge] = React.useState(24);
+  const [message, setMessage] = React.useState('random message');
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'get fucked' });
+    setMessage('hello world');
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      {/* <h3>{person.message}</h3> */}
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
